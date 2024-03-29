633678fde1e0fe414ba4ff6c1425ef77b48d558c
乐天居易 <19836788252@163.com>
2024-03-29 21:01:00 +0800

    yeb登录界面完成

---
+++ src/views/test.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="false"
+                    placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="password" v-model="loginForm.password" auto-complete="false"
+                    placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item prop="code">
+                <el-input size="normal" type="text" v-model="loginForm.code" auto-complete="false"
+                    placeholder="点击图片更换验证码" style="width: 250px;margin-right: 5px"></el-input>
+                <img :src="captchaUrl">
+            </el-form-item>
+            <el-checkbox class="loginRemember" v-model="checked">记住我</el-checkbox>
+                <el-button type="primary" style="width: 100%" @click="submitLogin">
+                    登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<style>
+.loginContainer {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 15px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+}
+
+.loginTitle {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+}
+
+.loginRemember {
+    text-align: left;
+    margin: 0px 0px 15px 0px;
+}
+
+.el-form-item__content {
+    display: flex;
+    align-items: center;
+}
+</style>
